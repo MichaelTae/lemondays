300c599b23234968ed0df91000bf32b92608d452
Michael S <Michael.Stojanovic@hotmail.com>
2023-05-23 18:21:42 +0200

    added banner

---
+++ src/components/Banner/lemonDaysBanner.jsx
@@ -0,0 +1,11 @@
+import lemonDays from '../../assets/le_mondays_logo_web_may2023.png'
+import './Banner.css'
+const LemonDaysBanner = () => {
+    return(
+        <div>
+          <img src={lemonDays} className='banner'  />
+        </div>
+    )
+};
+
+export default LemonDaysBanner;
