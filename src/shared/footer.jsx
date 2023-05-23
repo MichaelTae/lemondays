2d6b492f94b14065f106dedf59e99478dfbf7b72
Michael S <Michael.Stojanovic@hotmail.com>
2023-05-23 18:21:22 +0200

    added footer

---
+++ src/shared/footer.jsx
@@ -0,0 +1,15 @@
+import './footer.css'
+
+
+
+const Footer = () => {
+    return(
+        <footer className="footerContainer">
+            <div className='footerInc'>
+                <p>© 2023 Lemon Days</p>
+            </div>
+            </footer>
+    )
+};
+
+export default Footer;
